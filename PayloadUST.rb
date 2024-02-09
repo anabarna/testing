ff05524e91ae6af40ab7f9d5cf8b66f2b2f921b1
nyaoster <157842878+nyaoster@users.noreply.github.com>
2024-02-09 10:21:52 -0700

    Spacing changed

--- PayloadUST.rb
+++ PayloadUST.rb
@@ -14,13 +14,13 @@
 def PayloadUST(start_time, mode, duration = nil)
 
   def get_sn()
-  dut = ENV['DUT']
-  if dut == 'FSX_ATB'
-     device_sn = '21458177'
-  elsif dut == 'FSX_FM'
-     device_sn = '20323101'
-  end
-  return device_sn
+    dut = ENV['DUT']
+    if dut == 'FSX_ATB'
+        device_sn = '21458177'
+    elsif dut == 'FSX_FM'
+      device_sn = '20323101'
+    end
+    return device_sn
   end
   
     begin
